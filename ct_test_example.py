1dc8c68daf4bb6dcb34348505a8fbd125893120d
Christina <chc72@cam.ac.uk>
2021-05-19 10:46:37 +0100

    Test_4

--- ct_test_example.py
+++ ct_test_example.py
@@ -9,6 +9,7 @@ from ct_lib import *
 from scan_and_reconstruct import *
 from create_dicom import *
 import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
 
 # create object instances
 material = Material()
@@ -33,7 +34,6 @@ def test_1():
 
 	# how to check whether these results are actually correct?
 	
-
 	error = y - p
 	max_index = np.unravel_index(y.argmax(), y.shape)
 
@@ -43,6 +43,8 @@ def test_1():
 
 	plt.plot(p[63, :])
 	save_plot(y[63,:], 'results', 'test_1_plot')
+
+	
 
 def test_2():
 	# explain what this test is for
@@ -72,11 +74,60 @@ def test_3():
 
 	# how to check whether these results are actually correct?
 
+def test_4():
+	#Plot a 3D surface of a point
+	p = ct_phantom(material.name, 256, 2, 'Titanium')
+	s = fake_source(material.mev, 0.12, material.coeff('Aluminium'), 2, method='ideal')
+	scan = scan_and_reconstruct(s, material, p, 0.1, 256)
+
+	#Find average error of each layer around point attenuator
+	max_index = np.unravel_index(scan.argmax(), scan.shape)
+
+	scan /= np.max(scan)
+	scan *= 7
+	'''draw(scan)'''
+	error = abs(scan - p)
+	'''draw(error)'''
+
+	last_sum = 0
+	avg_error = 1
+	
+	for n in range(1,10):
+		if avg_error < 0.1:
+			break
+		layer = (error[(max_index[0]-n):(max_index[0]+n+1), (max_index[1]-n):(max_index[1]+n+1)])
+		avg_error = (np.sum(layer)-last_sum)/ (((2*n +1)**2) - (2*n-1)**2)
+		last_sum = np.sum(layer)
+		print(avg_error)
+
+	#Plot on 3D axis
+	x_axis = np.linspace(0, 256, 256)
+	y_axis = np.linspace(0, 256, 256)
+
+	X, Y = np.meshgrid(x_axis, y_axis)
+	Z1 = atten_values
+	Z2 = p
+	fig = plt.figure()
+	ax = plt.axes(projection='3d')
+	ax.plot_surface(X, Y, Z1, rstride=1, cstride=1,
+			 edgecolor='none')
+	#ax.plot_surface(X, Y, Z2, rstride=1, cstride=1,
+			 #edgecolor='none')
+	#plt.show()
+
+
+	#print(np.nonzero(p))
+	#print(np.nonzero(atten_values))
 
 # Run the various tests
-print('Test 1')
+''' print('Test 1')
 test_1()
-# print('Test 2')
-# test_2()
-# print('Test 3')
-# test_3()
+print('Test 2')
+test_2()
+print('Test 3')
+test_3() '''
+print('Test 4')
+test_4()
+
+#Find the max index
+#max_index = np.unravel_index(y.argamax(), y.shape)
